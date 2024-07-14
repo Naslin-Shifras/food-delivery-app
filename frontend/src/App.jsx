6d489e5df2c872e3ed6a0cadbefafc6832fd8459
Naslin-Shifras <naslinshifras@gmail.com>
2024-07-15 01:01:50 +0530

    error fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import Footer from './components/Footer/Footer';
-import PlaceOrder from './pages/PaceOrder/PlaceOrder';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import LoginPopup from './components/Log/LoginPopup';
 
 
